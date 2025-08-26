403a5721b9616a232926111a02c5cc90e416906d
IanOlmedo <i.olmedo@alumno.um.edu.ar>
2025-08-26 17:42:29 -0300

    name y edad inputs

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -4,15 +4,20 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,22 +27,44 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
+        var name: String by remember { mutableStateOf("")}
+        var edad: String by remember { mutableStateOf("") }
         Column(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.primarycontainer)
-            .safeContentPadding()
-            .fillMaxSize(),
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
 
-            horizontalAligment = Aligment.CenterHorizontally()
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text {
-                text = 'Hola Mundo'
+            TextField(
+                value = name,
+                onValueChange =  {
+                    name = it
+                }
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(name.isNotEmpty()){
+                Text(
+                    text = "Version Animada $name",
+                    fontSize = 20.sp
+                )
+
             }
-            Text {
-                text = 'Programacion 2'
-            }
-            Text {
-                text = 'Soy e mejor'
+            TextField(
+                value = edad,
+                onValueChange =  {
+                    edad = it
+                }
+            )
+
+            AnimatedVisibility(edad.isNotEmpty()){
+                Text(
+                    text = "Tu edad es: $edad",
+                    fontSize = 20.sp
+                )
             }
         }
     }
